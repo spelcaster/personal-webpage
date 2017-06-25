05177e996766716d529cc588955079cedd9c2c48
Hugo do Carmo <hugrito.kun@gmail.com>
2017-06-25 19:18:15 -0300

    initial commit
    - Added webpack config file;
    - Added initial content;
    - Added package.json and yarn.lock files;

---
+++ webpack.config.babel.js
@@ -0,0 +1,51 @@
+import path from 'path'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'style/bundle.css',
+  disable: process.env.NODE_ENV === 'development'
+})
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: extractSass.extract(
+          {
+            use: [
+              {
+                loader: 'css-loader'
+              },
+              {
+                loader: 'sass-loader'
+              }
+            ],
+            // use style-loader in development
+            fallback: 'style-loader'
+          }
+        )
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: false,
+            presets: ['es2015', 'stage-0']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    extractSass
+  ]
+}

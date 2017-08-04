42757819435435bd59184d98f0a736adce9e9110
Hugo do Carmo <hugrito.kun@gmail.com>
2017-08-03 23:53:51 -0300

    refactoring webpack config
    - Moved the rules to a const object, in order to have a cleaner module
      config;
    - Added the publicPath property to the output settings;

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,52 +6,65 @@ const extractSass = new ExtractTextPlugin({
   disable: process.env.NODE_ENV === 'development'
 })
 
+const rules = {
+  sass: {
+    test: /\.scss$/,
+    use: extractSass.extract(
+      {
+        use: [
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+        // use style-loader in development
+        fallback: 'style-loader'
+      }
+    )
+  },
+  fonts: {
+    test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+    loader: 'file-loader?name=fonts/[name].[ext]'
+
+  },
+  jsLint: {
+    enforce: 'pre',
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    loader: 'standard-loader',
+    options: {
+      parser: 'babel-eslint'
+    }
+  },
+  jsTranspile: {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        babelrc: false,
+        presets: ['react', 'es2015', 'stage-0']
+      }
+    }
+  }
+}
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
+    publicPath: '/',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'source-map',
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: extractSass.extract(
-          {
-            use: [
-              {
-                loader: 'css-loader'
-              },
-              {
-                loader: 'sass-loader'
-              }
-            ],
-            // use style-loader in development
-            fallback: 'style-loader'
-          }
-        )
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'standard-loader',
-        options: {
-          parser: 'babel-eslint'
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            babelrc: false,
-            presets: ['es2015', 'stage-0', 'react']
-          }
-        }
-      }
+      rules.sass,
+      rules.fonts,
+      rules.jsLint,
+      rules.jsTranspile
     ]
   },
   plugins: [

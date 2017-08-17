08688e6fe5155265eeeb60d407633f92cfebcd73
Hugo do Carmo <hugrito.kun@gmail.com>
2017-08-17 19:03:21 -0300

    refactoring GithubSection component
    - This component now uses the Project component to display the
      repositories;
    - Added a state to assign the repositories found;

--- src/components/GithubSection.js
+++ src/components/GithubSection.js
@@ -1,29 +1,64 @@
 import React from 'react'
-// import axios from 'axios'
+import axios from 'axios'
 
-import GithubProject from './GithubProject'
+import Project from './Project'
 
 class GithubSection extends React.Component {
   state = {
-    apiUrl: 'https://api.github.com/users/spelcaster/repos'
+    apiUrl: 'https://api.github.com/users/spelcaster/repos',
+    repos: []
   }
 
   componentWillMount () {
-    /*
+    var self = this
     axios.get(this.state.apiUrl)
       .then(function (response) {
-        console.log(response)
+        // filter out forked repositories
+        self.getRepoList(
+          response.data.filter((repo) => { return !repo.fork })
+        )
       })
-      .catch(function (error) {
-        console.log(error)
-      })
-      */
+  }
+
+  getRepoList = (repos) => {
+    var projects = repos.map((repo) => {
+      return {
+        id: repo.id,
+        title: repo.name,
+        description: repo.description,
+        links: [
+          {
+            url: repo.html_url,
+            text: 'View project'
+          }
+        ],
+        language: repo.language,
+        stargazers_count: repo.stargazers_count,
+        is_popular: (repo.stargazers_count > 0)
+      }
+    })
+
+    this.setState({repos: projects})
+  }
+
+  renderProject = (project, index) => {
+    return (
+      <Project key={index}
+        id={index}
+        title={project.title}
+        description={project.description}
+        links={project.links}
+        colClass='col-sm-6' />
+    )
   }
 
   render () {
     return (
-      <div id='github-section' className='row'>
-        <GithubProject title='Project Title' />
+      <div id='github-section'>
+        <h2>Github</h2>
+        <div className='row'>
+          {this.state.repos.map(this.renderProject)}
+        </div>
       </div>
 
     )
